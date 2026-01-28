ac8b4ce6952c36e70e25248997fa27ac31b54f8f
Johannes Röttenbacher <jr-alias@posteo.de>
2026-01-28 14:41:59 +0100

    Linting

--- src/pelican/plugins/pybtex/utils.py
+++ src/pelican/plugins/pybtex/utils.py
@@ -170,33 +170,30 @@ def _get_month_number(m: str) -> int:
     return _MONTH_NUMBERS[m.lower()[:3].strip()]
 
 
-
 def _get_style(style_name):
     # If style_name is one of the known built-in simple names, import accordingly
     built_in_styles = {"plain", "alpha", "unsrt", "unsrtalpha"}
 
     if style_name in built_in_styles:
         formatter = importlib.import_module(f"pybtex.style.formatting.{style_name}")
-        style = formatter.Style()
-        return style
+        return formatter.Style()
 
     # Else try to import custom style given as full module path
     # e.g. style_name = "my_pybtex_styles.MyCustomStyle"
-    if '.' in style_name:
-        mod_name, class_name = style_name.rsplit('.', 1)
+    if "." in style_name:
+        mod_name, class_name = style_name.rsplit(".", 1)
         try:
             mod = importlib.import_module(mod_name)
             style_class = getattr(mod, class_name)
-            style = style_class()
-            return style
-        except (ModuleNotFoundError, AttributeError) as e:
-            logger.error(f"Failed to import custom style '{style_name}': {e}")
+            return style_class()
+        except (ModuleNotFoundError, AttributeError):
+            logger.exception(f"Failed to import custom style '{style_name}'")
 
     # Fallback to plain style on failure
     logger.error(f"Unsupported formatting style `{style_name}`, defaulting to `plain`")
     import pybtex.style.formatting.plain
-    style = pybtex.style.formatting.plain.Style()
-    return style
+
+    return pybtex.style.formatting.plain.Style()
 
 
 def generate_context(
