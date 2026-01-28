424c64085a2c7087b690c32bf48c29869194744b
Johannes Röttenbacher <jr-alias@posteo.de>
2026-01-28 14:40:11 +0100

    Add files for custom style test
    - publications.bib with two real bibtex entries containing the same name in different formats
    - pelicanconf.py with the custom style setting

---
+++ tests/data/custom-style/pelicanconf.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: Copyright © 2026 Johannes Röttenbacher <jr-alias@posteo.de>
+# SPDX-License-Identifier: MIT
+
+PATH = "content"
+PYBTEX_SOURCES = ["publications.bib"]
+PYBTEX_FORMAT_STYLE = "custom_bibtex_style.HighlightAuthors"
